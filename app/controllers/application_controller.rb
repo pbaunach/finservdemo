ed214965030f521677173f5af918600e767aca55
pbaunach <pbaunach@gmail.com>
2015-11-10 11:58:37 -0800

    Yolo
    Adding Collaborate to the Wealth Management Profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "FinServ", password: "123salesforce"
+  #http_basic_authenticate_with name: "FinServ", password: "123salesforce"
 end
